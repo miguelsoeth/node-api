c925269cf50c6cb89902735714e094d3f70defd9
Miguel Soeth <61883339+miguelsoeth@users.noreply.github.com>
2024-02-29 18:19:42 -0300

    end of day 29/02

---
+++ src/models/connection.js
@@ -0,0 +1,10 @@
+const mysql = require('mysql2/promise');
+
+const connection = mysql.createPool({
+    host: process.env.MARIADB_HOST,
+    user: process.env.MARIADB_USER,
+    password: process.env.MARIADB_PASSWORD,
+    database: process.env.MARIADB_DATABASE,
+});
+
+module.exports = connection;
--- src/router.js
+++ src/router.js
@@ -1,5 +1,8 @@
 const express = require('express');
+const tasksController = require('./controllers/tasksController');
 
 const router = express.Router();
+
+router.get('/tasks', tasksController.getAll);
 
 module.exports = router;
